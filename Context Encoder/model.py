7646ae6e26396932fda7d39d163d5ef13b9da25a
CSMYang <portal.yang@mail.utoronto.ca>
2021-03-28 20:01:31 -0400

    Some bugs are fixed

--- Context Encoder/model.py
+++ Context Encoder/model.py
@@ -9,58 +9,58 @@ import matplotlib.pyplot as plt
 
 class Generator(nn.module):
     def __init__(self, args):
-        super_(Generator, self).__init__()
+        super(Generator, self).__init__()
 
         # input size is image size of 3 x width x height
 
         # encoder layers
         self.encoder = nn.Sequential(
             # image size 3 -> 64
-            nn.Conv2d(3, 64, kernal=4, stide=2, padding=1),
-            nn.LeakyReLU(0.2, inplace=true),
+            nn.Conv2d(3, 64, kernel_size=4, stride=2, padding=1),
+            nn.LeakyReLU(0.2, inplace=True),
             #   64-> 64
-            nn.Conv2d(64, 64, kernal=4, stide=2, padding=1),
+            nn.Conv2d(64, 64, kernel_size=4, stride=2, padding=1),
             nn.BatchNorm2d(64),
-            nn.LeakyReLU(0.2, inplace=true),
+            nn.LeakyReLU(0.2, inplace=True),
             #   64-> 128
-            nn.Conv2d(64, 128, kernal=4, stide=2, padding=1),
+            nn.Conv2d(64, 128, kernel_size=4, stride=2, padding=1),
             nn.BatchNorm2d(128),
-            nn.LeakyReLU(0.2, inplace=true),
+            nn.LeakyReLU(0.2, inplace=True),
             # 128 -> 256
-            nn.Conv2d(128, 256, kernal=4, stide=2, padding=1),
+            nn.Conv2d(128, 256, kernel_size=4, stride=2, padding=1),
             nn.BatchNorm2d(256),
-            nn.LeakyReLU(0.2, inplace=true),
+            nn.LeakyReLU(0.2, inplace=True),
             # 256 -> 512
-            nn.Conv2d(256, 512, kernal=4, stide=2, padding=1),
+            nn.Conv2d(256, 512, kernel_size=4, stride=2, padding=1),
             nn.BatchNorm2d(512),
-            nn.LeakyReLU(0.2, inplace=true),
+            nn.LeakyReLU(0.2, inplace=True),
             # channel wise fully connected layer for the bottle neck layer  with stride 1 convolution
             # 512 -> 4000
-            nn.Conv2d(512, 4000, stide=1)
-            nn.BatchNorm2d(0.2, inplace=true),
+            nn.Conv2d(512, 4000, stride=1, ),
+            nn.BatchNorm2d(4000)
 
         )
         self.decoder = nn.Sequential(
             # 4000 -> 512
-            nn.ConvTranspose2d(4000, 512, kernal=4, stide=2, padding=1),
+            nn.ConvTranspose2d(4000, 512, kernel_size=4, stride=2, padding=1),
             nn.BatchNorm2d(512),
-            nn.ReLU(inplace=true),
+            nn.ReLU(inplace=True),
             # 512 -> 256
-            nn.ConvTranspose2d(512, 256, kernal=4, stide=2, padding=1),
+            nn.ConvTranspose2d(512, 256, kernel_size=4, stride=2, padding=1),
             nn.BatchNorm2d(256),
-            nn.ReLU(inplace=true),
+            nn.ReLU(inplace=True),
             # 256 -> 128
-            nn.ConvTranspose2d(256, 128, kernal=4, stide=2, padding=1),
+            nn.ConvTranspose2d(256, 128, kernel_size=4, stride=2, padding=1),
             nn.BatchNorm2d(128),
-            nn.ReLU(inplace=true),
+            nn.ReLU(inplace=True),
             # 128 -> 64
-            nn.ConvTranspose2d(128, 64, kernal=4, stide=2, padding=1),
+            nn.ConvTranspose2d(128, 64, kernel_size=4, stride=2, padding=1),
             nn.BatchNorm2d(64),
-            nn.ReLU(inplace=true),
+            nn.ReLU(inplace=True),
             # 64 -> 3
             # nn.ConvTranspose2d(),
-            nn.ConvTranspose2d(64, 3, kernal=4, stide=2, padding=1),
-            nn.Tanh(inplace=true)
+            nn.ConvTranspose2d(64, 3, kernel_size=4, stride=2, padding=1),
+            nn.Tanh(inplace=True)
         )
 
     def forward(self, x):
@@ -71,28 +71,28 @@ class Generator(nn.module):
 
 class Discriminator(nn.module):
     def __init__(self, args):
-        super_(Generator, self).__init__()
+        super(Generator, self).__init__()
         self.layers = nn.Sequential(
             # 3 -> 64
-            nn.Conv2d(3, 64, kernal=4, stide=2, padding=1),
-            nn.LeakyReLU(0.2, inplace=true),
+            nn.Conv2d(3, 64, kernel_size=4, stride=2, padding=1),
+            nn.LeakyReLU(0.2, inplace=True),
             # 64 -> 128
-            nn.Conv2d(64, 128, kernal=4, stide=2, padding=1),
+            nn.Conv2d(64, 128, kernel_size=4, stride=2, padding=1),
             nn.BatchNorm2d(128),
-            nn.LeakyReLU(0.2, inplace=true),
+            nn.LeakyReLU(0.2, inplace=True),
             # 128 -> 256
-            nn.Conv2d(128, 256, kernal=4, stide=2, padding=1),
+            nn.Conv2d(128, 256, kernel_size=4, stride=2, padding=1),
             nn.BatchNorm2d(256),
-            nn.LeakyReLU(0.2, inplace=true),
+            nn.LeakyReLU(0.2, inplace=True),
             # 256 -> 512
-            nn.Conv2d(256, 512, kernal=4, stide=2, padding=1),
+            nn.Conv2d(256, 512, kernel_size=4, stride=2, padding=1),
             nn.BatchNorm2d(512),
-            nn.LeakyReLU(0.2, inplace=true),
+            nn.LeakyReLU(0.2, inplace=True),
 
-            # or kernal=3, stide=1, padding=1??
-            nn.Conv2d(512, 1, kernal=4, stide=1, padding=0),
+            # or kernel_size=3, stride=1, padding=1??
+            nn.Conv2d(512, 1, kernel_size=4, stride=1, padding=0),
             nn.Sigmoid()
         )
 
-    def forward(x):
+    def forward(self, x):
         return self.layer(x)
