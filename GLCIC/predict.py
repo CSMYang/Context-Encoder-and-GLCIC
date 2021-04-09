c6ee433071af36e4df493155ce414e5815bf3329
apple1 <chengjiahao1234@live.com>
2021-04-09 15:33:35 -0400

    add code that works for fid

--- GLCIC/predict.py
+++ GLCIC/predict.py
@@ -1,10 +1,12 @@
 import json
+import os
 import torch
 import torchvision.transforms as transforms
 from torchvision.utils import save_image
 from PIL import Image
 from model import CompletionNetwork
 from train import poisson_blend, generate_mask
+from dataset import ImageDataset
 
 
 class AttrDict(dict):
@@ -13,14 +15,55 @@ class AttrDict(dict):
         self.__dict__ = self
 
 
+def test_images(args, mpv, model):
+    """
+    Predict imgs from dir
+    """
+    # convert img to tensor
+    transformed = transforms.Compose([
+        transforms.Resize(args.img_size),
+        transforms.RandomCrop((args.img_size, args.img_size)),
+        transforms.ToTensor(),
+    ])
+    image_set = ImageDataset(os.path.join(args.data_dir, 'train'), transformed)
+    mask_path = args.output_img + "/mask"
+    output_path = args.output_img + "/output"
+
+    for i in range(image_set):
+        x = torch.unsqueeze(image_set[i], dim=0)
+
+        # create mask
+        mask = generate_mask(
+            shape=(1, 1, x.shape[2], x.shape[3]),
+            hole_size=(
+                (args.hole_min_w, args.hole_max_w),
+                (args.hole_min_h, args.hole_max_h),
+            ),
+            max_holes=args.max_holes,
+        )
+
+        # inpaint
+        model.eval()
+        with torch.no_grad():
+            x_mask = x - x * mask + mpv * mask
+            input = torch.cat((x_mask, mask), dim=1)
+            output = model(input)
+            inpainted = poisson_blend(x_mask, output, mask)
+            maskpath = os.path.join(mask_path, 'test_%d.png' % i)
+            outputpath = os.path.join(output_path, 'test_%d.png' % i)
+            save_image(imgs, maskpath)
+            save_image(inpainted, outputpath)
+    print('output img was saved as %s.' % args.output_img)
+
+
 if __name__ == "__main__":
     args = AttrDict()
     # set hyperparameters
     args_dict = {
         "model": "./img_align_celeba/",
         "config": "./results/result/",
-        "input_img": "", # input img location
-        "output_img": "", # output img location
+        "input_img": "", # input img
+        "output_img": "", # output img name
         "max_holes": 5,
         "img_size": 160,
         "hole_min_w": 24,
@@ -67,5 +110,5 @@ if __name__ == "__main__":
         output = model(input)
         inpainted = poisson_blend(x_mask, output, mask)
         imgs = torch.cat((x, x_mask, inpainted), dim=0)
-        save_image(inpainted, args.output_img, nrow=3)
+        save_image(imgs, args.output_img, nrow=3)
     print('output img was saved as %s.' % args.output_img)
