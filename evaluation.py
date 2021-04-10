e3c314445488a7fb04602a3078e55b5a7372a88e
CSMYang <portal.yang@mail.utoronto.ca>
2021-04-09 21:02:32 -0400

    PSRN modified

--- evaluation.py
+++ evaluation.py
@@ -1,20 +1,21 @@
 import numpy
 import math
-from skimage.metrics import structural_similarity, peak_signal_noise_ratio
-import torch
+from skimage.metrics import structural_similarity
 
 
-def psnr(real_img, generated_img):
+def psnr(real_img, generated_img, PIXEL_MAX = 255.0):
     """
     This function computes the PSNR score between two images.
     :param generated_img: A tensor to the generated image
     :param real_img: A tensor to the real image
     :return: The PSNR score
     """
-    img_1 = real_img.numpy()
-    img_2 = generated_img.numpy()
-
-    return peak_signal_noise_ratio(img_1, img_2, data_range=real_img.max() - real_img.min())
+    img_1 = real_img.cpu().numpy()
+    img_2 = generated_img.cpu().numpy()
+    mse = numpy.mean((img_1 - img_2) ** 2)
+    if mse == 0:
+        return 100
+    return 20 * math.log10(PIXEL_MAX / math.sqrt(mse))
 
 
 def ssim(real_img, generated_img):
@@ -24,7 +25,7 @@ def ssim(real_img, generated_img):
     :param generated_img: A tensor to the real image
     :return: The structural similarity score
     """
-    img_1 = real_img.numpy()
-    img_2 = generated_img.numpy()
+    img_1 = real_img.cpu().numpy()
+    img_2 = generated_img.cpu().numpy()
 
     return structural_similarity(img_1, img_2, data_range=real_img.max() - real_img.min())
