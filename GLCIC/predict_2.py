071c1a5bd1bb2205ab664a20eb9d83b2c53b0501
apple1 <chengjiahao1234@live.com>
2021-04-11 21:07:49 -0400

    write predict for subtitle removal

---
+++ GLCIC/predict_2.py
@@ -0,0 +1,114 @@
+import json
+import os
+import torch
+import torchvision.transforms as transforms
+from torchvision.utils import save_image
+from PIL import Image
+from model_pretrained import CompletionNetwork
+from train import poisson_blend
+from dataset import ImageDataset
+from detect_subtitle import get_area, generate_mask
+
+
+class AttrDict(dict):
+    def __init__(self, *args, **kwargs):
+        super(AttrDict, self).__init__(*args, **kwargs)
+        self.__dict__ = self
+
+
+def test_images(args, mpv, model):
+    """
+    Predict imgs from dir
+    """
+    # convert img to tensor
+    transformed = transforms.Compose([
+        transforms.Resize(args.img_size),
+        transforms.RandomCrop((args.img_size, args.img_size)),
+        transforms.ToTensor(),
+    ])
+    image_set = ImageDataset(os.path.join(args.data_dir, 'train'), transformed)
+    mask_path = args.output_img + "/mask"
+    output_path = args.output_img + "/output"
+
+    for i in range(image_set):
+        x = torch.unsqueeze(image_set[i], dim=0)
+
+        # create mask
+        mask = generate_mask(
+            shape=(1, 1, x.shape[2], x.shape[3]),
+            hole_size=(
+                (args.hole_min_w, args.hole_max_w),
+                (args.hole_min_h, args.hole_max_h),
+            ),
+            max_holes=args.max_holes,
+        )
+
+        # inpaint
+        model.eval()
+        with torch.no_grad():
+            x_mask = x - x * mask + mpv * mask
+            input = torch.cat((x_mask, mask), dim=1)
+            output = model(input)
+            inpainted = poisson_blend(x_mask, output, mask)
+            maskpath = os.path.join(mask_path, 'test_%d.png' % i)
+            outputpath = os.path.join(output_path, 'test_%d.png' % i)
+            save_image(imgs, maskpath)
+            save_image(inpainted, outputpath)
+    print('output img was saved as %s.' % args.output_img)
+
+
+if __name__ == "__main__":
+    args = AttrDict()
+    # set hyperparameters
+    args_dict = {
+        "model": "GLCIC\pretrained_model_cn",
+        "config": "GLCIC\config.json",
+        "input_img": "img_align_celeba\\test\\000003.jpg",  # input img
+        "output_img": "GLCIC\\result.jpg",  # output img name
+        "max_holes": 5,
+        "img_size": 160,
+        "hole_min_w": 24,
+        "hole_max_w": 46,
+        "hole_min_h": 24,
+        "hole_max_h": 48,
+    }
+    args.update(args_dict)
+
+    # =============================================
+    # Load model
+    # =============================================
+    with open(args.config, 'r') as f:
+        config = json.load(f)
+    mpv = torch.tensor(config['mpv']).view(1, 3, 1, 1)
+    model = CompletionNetwork()
+    model.load_state_dict(torch.load(args.model, map_location='cpu'))
+
+    # =============================================
+    # Predict
+    # =============================================
+    # convert img to tensor
+    img = Image.open(args.input_img)
+    img = transforms.Resize(args.img_size)(img)
+    # img = transforms.RandomCrop((args.img_size, args.img_size))(img)
+    x = transforms.ToTensor()(img)
+    x = torch.unsqueeze(x, dim=0)
+
+    # create mask
+    temp_path = "./test.png"
+    save_image(x, temp_path, nrow=1)
+    mask = generate_mask(
+        shape=(1, 1, x.shape[2], x.shape[3]),
+        area=get_area(temp_path)
+    )
+    os.remove(temp_path)
+
+    # inpaint
+    model.eval()
+    with torch.no_grad():
+        x_mask = x - x * mask + mpv * mask
+        input = torch.cat((x_mask, mask), dim=1)
+        output = model(input)
+        inpainted = poisson_blend(x_mask, output, mask)
+        imgs = torch.cat((x, x_mask, inpainted), dim=0)
+        save_image(imgs, args.output_img, nrow=3)
+    print('output img was saved as %s.' % args.output_img)
